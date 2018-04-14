cc4ab6c8ed779a5e0cfa0ea14a36c906e01aeda0
Jesse Olson <jesse@arkham.city>
2018-04-14 13:24:14 -0500

    Make my bob answer far less shitty

--- bob/bob.go
+++ bob/bob.go
@@ -2,55 +2,31 @@
 package bob
 
 import (
-	"regexp"
+	"strings"
 )
+
+func yelly(s string) bool {
+	return s == strings.ToUpper(s) && s != strings.ToLower(s)
+}
+
+func questiony(s string) bool {
+	return strings.HasSuffix(s, "?")
+}
 
 // Hey is bob's personality function. "Siri for teenagers"
 func Hey(remark string) string {
-	var response string
+	remark = strings.TrimSpace(remark)
 
-	yellingQuestion, _ := regexp.Compile(`[A-Z]+\?`)
-	question, _ := regexp.Compile(`\?(\s+)?$`)
-
-	// if every character in the string is uppercase and there is a question mark at the end
-	if isYelling(remark) && yellingQuestion.MatchString(remark) {
-		response = "Calm down, I know what I'm doing!"
-		// if every character in the string is uppercase
-	} else if isYelling(remark) {
-		response = "Whoa, chill out!"
-		// if the string contains a question mark at the end
-	} else if question.MatchString(remark) {
-		response = "Sure."
-	} else if nothing(remark) {
-		response = "Fine. Be that way!"
-	} else {
-		response = "Whatever."
+	switch {
+	case remark == "":
+		return "Fine. Be that way!"
+	case yelly(remark) && questiony(remark):
+		return "Calm down, I know what I'm doing!"
+	case yelly(remark):
+		return "Whoa, chill out!"
+	case questiony(remark):
+		return "Sure."
+	default:
+		return "Whatever."
 	}
-
-	return response
-}
-
-// IS THIS YELLING? TRUE.
-func isYelling(s string) bool {
-	onlyUpper, _ := regexp.Compile("[^A-Z]+")
-	upperAndLower, _ := regexp.Compile("[^A-Za-z]+")
-
-	upperString := onlyUpper.ReplaceAllString(s, "")
-	upperLowerString := upperAndLower.ReplaceAllString(s, "")
-
-	// No characters? No yelling.
-	if upperLowerString == "" {
-		return false
-	}
-
-	return upperString == upperLowerString
-}
-
-// Simply return true if the string is empty
-func nothing(s string) bool {
-	upperAndLower, _ := regexp.Compile(`[^\s]+`)
-
-	nothingString := upperAndLower.ReplaceAllString(s, "")
-
-	return s == nothingString
 }
